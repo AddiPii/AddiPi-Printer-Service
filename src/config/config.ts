b70b264f84d1ac4d8cac7d76350f6d0ff742cec4
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-28 20:51:37 +0100

    Poprawiono zmienną IOT_HUB_SERVICE_CS, aby używała poprawnej wartości z env zamiast JWT_REFRESH_SECRET.

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,7 +11,7 @@ const getRequired = (name: string) => {
 };
 
 export const CONFIG: configType = {
-    IOT_HUB_SERVICE_CS: getRequired('JWT_REFRESH_SECRET'),
+    IOT_HUB_SERVICE_CS: getRequired('IOT_HUB_SERVICE_CS'),
     COSMOS_ENDPOINT: getRequired('COSMOS_ENDPOINT'),
     COSMOS_KEY: getRequired('COSMOS_KEY'),
     PORT: Number(process.env.AUTH_PORT || 3050),
