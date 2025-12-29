36f5c0da2de1e7273b4d895c905a10a43b0c798f
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-29 01:52:10 +0100

    feat: implement TelemetryListener class for handling telemetry messages

--- src/services/telemetryListener.ts
+++ src/services/telemetryListener.ts
@@ -2,5 +2,25 @@ import { EventHubConsumerClient } from "@azure/event-hubs";
 import type { Container } from "@azure/cosmos";
 import { initCosmosContainer } from "../db/initCosmosContainer.js";
 import { CONFIG } from "../config/config.js";
+import type { TelemetryMessage } from "../type.js";
 
 
+export class TelemetryListener {
+    private consumerClient: EventHubConsumerClient
+    private container: Container
+    private isRunning: boolean = false
+
+    constructor(eventHubConnectionString: string){
+        this.consumerClient = new EventHubConsumerClient(
+            '$Default',
+            eventHubConnectionString
+        )
+
+        this.container = initCosmosContainer(\
+            CONFIG.COSMOS_ENDPOINT,
+            CONFIG.COSMOS_KEY
+        )
+        
+        console.log('Telemetry Listener initialized')
+    }
+}
