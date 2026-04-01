107330347087cae5d8bfd9a9bd19e8a64e74a0bd
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-04-01 21:35:56 +0200

    add new config vars to use webhooks and alert emails in the future

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,10 +11,12 @@ const getRequired = (name: string) => {
 };
 
 export const CONFIG: configType = {
-    IOT_HUB_SERVICE_CS: getRequired('IOT_HUB_SERVICE_CS'),
-    IOT_HUB_EVENT_HUB_CS: getRequired('IOT_HUB_EVENT_HUB_CS'),
-    COSMOS_ENDPOINT: getRequired('COSMOS_ENDPOINT'),
-    COSMOS_KEY: getRequired('COSMOS_KEY'),
-    AUTH_SERVICE_URL: getRequired('AUTH_SERVICE_URL') || 'http://host.docker.internal:3001',
-    PORT: Number(process.env.PRINTER_PORT || 3050),
+  IOT_HUB_SERVICE_CS: getRequired('IOT_HUB_SERVICE_CS'),
+  IOT_HUB_EVENT_HUB_CS: getRequired('IOT_HUB_EVENT_HUB_CS'),
+  COSMOS_ENDPOINT: getRequired('COSMOS_ENDPOINT'),
+  COSMOS_KEY: getRequired('COSMOS_KEY'),
+  AUTH_SERVICE_URL: getRequired('AUTH_SERVICE_URL') || 'http://host.docker.internal:3001',
+  ALERT_WEBHOOK_URL: process.env.ALERT_WEBHOOK_URL,
+  ADMIN_ALERT_EMAILS: process.env.ADMIN_ALERT_EMAILS,
+  PORT: Number(process.env.PRINTER_PORT || 3050),
 };
